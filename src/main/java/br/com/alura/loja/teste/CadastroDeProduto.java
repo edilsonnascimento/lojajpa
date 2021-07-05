3274ff81cf3b454e8d6bfec0bafe5eef8132bd97
edilsonnascimento <edilsonjoper@gmail.com>
2021-07-05 13:43:13 -0300

    05 - Organizando Código: criado DAO e JPAUtil.

--- src/main/java/br/com/alura/loja/teste/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/teste/CadastroDeProduto.java
@@ -1,2 +1,30 @@
-package br.com.alura.loja.modelo;public class CadastroDeProduto {
+package br.com.alura.loja.teste;
+
+import br.com.alura.loja.dao.ProdutoDao;
+import br.com.alura.loja.modelo.Produto;
+import br.com.alura.loja.util.JPAUtil;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.math.BigDecimal;
+
+public class CadastroDeProduto {
+
+    public static void main(String[] args) {
+        Produto celular = new Produto();
+        celular.setNome("Xiomi Redmi");
+        celular.setDescricao("Muto legal");
+        celular.setPreco(new BigDecimal("800"));
+
+        EntityManager em = JPAUtil.getFactory();
+        ProdutoDao produtoDao = new ProdutoDao(em);
+
+        em.getTransaction().begin();
+        em.persist(celular);
+        em.getTransaction().commit();
+
+        em.close();
+
+    }
 }
